c473af56844390821077f3ba692f27a326e6653f
Erik Jagyugya <erik.jagyugya@origoss.com>
2020-04-03 17:08:22 +0200

    Add project skeleton

---
+++ qommunicator/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for Qommunicatorr"""
+
+import pbr.version
+
+__author__ = 'Erik Jagyugya'
+__email__ = 'jagyugyaerik@gmail.com'
+
+version_info = pbr.version.VersionInfo('qommunicator`')
+__version__ = version_info.release_string()
